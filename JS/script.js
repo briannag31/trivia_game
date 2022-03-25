3c5b8b167f6255eb996c0d1d371a0ec7f1bd25e3
Brianna Gaines <briannagaines31@gmail.com>
2022-03-24 18:07:32 -0600

    user story added as outline for JS

---
+++ JS/script.js
@@ -0,0 +1,25 @@
+// User clicks let's play
+//team one selects category
+//category click value data to API url
+//first API call gives us the first question
+// Loop:
+// Team one adds their answer and clicks submit
+// Get value of the text box and compare it to the correct answer
+// Text box is cleared
+// If correct answer, give team one point (render); if incorrect, alert wrong answer
+
+// 	Pop up box noting it is team two’s turn - how to do this?
+// Click next question
+//team two selects a category
+// API call for question
+// Team two adds their answer and clicks submit
+// Get value of the text box and compare it to the correct answer
+//Text box is cleared
+// If correct answer, give team two point (render); if incorrect, alert wrong answer
+// 	Pop up box noting it is team one's turn - how to do this?
+// Loop ends when one of the teams gets to 5
+// 	Pop up box saying which team wins!
+// 	Restart game button 
+
+
+
